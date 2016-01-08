014d44647f6816d36190accedd4e902b9c7af577
Matteo Cerutti <matteo.cerutti@swisscom.com>
2016-01-08 15:55:56 +0100

    defaulting to 2015-07-25 api version

--- rancher_metadata/__init__.py
+++ rancher_metadata/__init__.py
@@ -42,12 +42,12 @@ class MetadataAPI:
           success = True
           break
         except Exception as e:
-          print("Failed to query Rancher Metadata API on " + url + " - Caught exception (" + str(e) + ")")
+          print("Failed to query Rancher Metadata API on %s - Caught exception (%s)" % (url, str(e)))
 
       i = i + 1
 
     if not success:
-      raise RuntimeError("Failed to query Rancher Metadata API (" + str(i) + " out of " + str(self.max_attempts) + " attempts failed)")
+      raise RuntimeError("Failed to query Rancher Metadata API (%d out of %d attempts failed)" % (i, self.max_attempts))
 
     if self.is_error(req):
       return None
@@ -65,7 +65,7 @@ class MetadataAPI:
         raise ValueError("Attribute 'service_name' is required")
 
       if 'stack_name' not in kwargs:
-        return self.api_get("/services/" + kwargs['service_name'])
+        return self.api_get("/services/%s" % kwargs['service_name'])
       else:
         for s in self.get_services():
           if s['stack_name'] == kwargs['stack_name'] and s['name'] == kwargs['service_name']:
@@ -73,13 +73,13 @@ class MetadataAPI:
 
   def get_service_field(self, field, **kwargs):
     if not kwargs:
-      return self.api_get("/self/service/" + field)
+      return self.api_get("/self/service/%s" % field)
     else:
       if 'service_name' not in kwargs:
         raise ValueError("Attribute 'service_name' is required")
 
       if 'stack_name' not in kwargs:
-        return self.api_get("/services/" + kwargs['service_name'] + "/" + field)
+        return self.api_get("/services/%s/%s" % (kwargs['service_name'], field))
       else:
         s = self.get_service(**kwargs)
         if s and field in s:
@@ -123,7 +123,7 @@ class MetadataAPI:
     if stack_name is None:
       return self.api_get("/self/stack")
     else:
-      return self.api_get("/stacks/" + stack_name)
+      return self.api_get("/stacks/%s" % stack_name)
 
   def get_containers(self):
     return self.api_get("/containers")
@@ -132,16 +132,16 @@ class MetadataAPI:
     if container_name is None:
       return self.api_get("/self/container")
     else:
-      return self.api_get("/containers/" + container_name)
+      return self.api_get("/containers/%s" % container_name)
 
   def get_container_field(self, field, container_name):
     if container_name is None:
-      return self.api_get("/self/container/" + field)
+      return self.api_get("/self/container/%s" % field)
     else:
-      return self.api_get("/containers/" + container_name + "/" + field)
+      return self.api_get("/containers/%s/%s" % (container_name, field))
 
   def get_container_id(self, container_name = None):
-    return self.get_container_field("create_index", container_name)
+    return int(self.get_container_field("create_index", container_name))
 
   def get_container_ip(self, container_name = None):
     if container_name is None:
@@ -151,7 +151,7 @@ class MetadataAPI:
       else:
         return self.get_host_ip()
     else:
-      return self.api_get("/containers/" + container_name + "/primary_ip")
+      return self.api_get("/containers/%s/primary_ip" % container_name)
 
   def get_container_name(self, container_name = None):
     return self.get_container_field("name", container_name)
@@ -198,13 +198,13 @@ class MetadataAPI:
     if host_name is None:
       return self.api_get("/self/host")
     else:
-      return self.api_get("/hosts/" + host_name)
+      return self.api_get("/hosts/%s" % host_name)
 
   def get_host_field(self, field, host_name):
     if host_name is None:
-      return self.api_get("/self/host/" + field)
+      return self.api_get("/self/host/%s" % field)
     else:
-      return self.api_get("/hosts/" + host_name + "/" + field)
+      return self.api_get("/hosts/%s/%s" % (host_name, field))
 
   def get_host_ip(self, host_name = None):
     return self.get_host_field("agent_ip", host_name)
