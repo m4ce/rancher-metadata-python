e11ea6707334f5250f5dac793a523074b93516b7
Matteo Cerutti <matteo.cerutti@swisscom.com>
2016-01-28 15:53:01 +0100

    renamed to rancher-metadata

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from distutils.core import setup
 
-version = '0.17.1'
+version = '0.17.2'
 
 setup(
-  name = 'rancher_metadata',
+  name = 'rancher-metadata',
   packages = ['rancher_metadata'],
   version = version,
   description = 'Python library for Rancher Metadata API',
   author = 'Matteo Cerutti',
   author_email = 'matteo.cerutti@hotmail.co.uk',
-  url = 'https://github.com/m4ce/python-rancher_metadata',
-  download_url = 'https://github.com/m4ce/python-rancher_metadata/tarball/%s' % (version,),
+  url = 'https://github.com/m4ce/rancher-metadata-python',
+  download_url = 'https://github.com/m4ce/rancher-metadata-python/tarball/%s' % (version,),
   keywords = ['rancher'],
   classifiers = [],
   install_requires = ["requests"]
